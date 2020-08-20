a219f1bc93eddfc7d7fa8c54ac2a6883e8ed6afe
IIyDeJIb <gasimov.p@gmail.com>
2020-08-20 01:20:58 -0500

    started working on treatment of repeated wells within one failInfo string

--- buildActMat.py
+++ buildActMat.py
@@ -1,11 +1,13 @@
-import pandas as pd
 import logging
-from fullWellList import fullWellList
-from xAndClassFailures import xAndClassFailures
-from customErrors import NoWellsFoundError, RepeatedWellsError
-from procFailSeries import procFailSeries
 # from pg_pandas import closeSeries, compFillna
 from datetime import datetime
+
+import pandas as pd
+
+from customErrors import NoWellsFoundError
+from fullWellList import fullWellList
+from procFailSeries import procFailSeries
+from xAndClassFailures import xAndClassFailures
 
 # from openpyxl import load_workbook
 # import os
@@ -15,7 +17,8 @@ from datetime import datetime
 logger = logging.getLogger(__name__)
 logger.setLevel('DEBUG')
 
-formatter = '%(asctime)s : %(name)s : %(funcName)s : %(levelname)s -> %(message)s'
+formatter = logging.Formatter('%(asctime)s : %(name)s : %('
+							  'levelname)s -> %(message)s')
 
 # File logging handler
 fh = logging.FileHandler('actMat.log')
@@ -33,11 +36,13 @@ logger.addHandler(ch)
 
 logger.debug('----- Start of Script -----')
 
-failTypeCode = {'subs': -1,
-				'surf': 0,
-				'uncl': -2,
-				'op': 1,
-				'tbd': -3}
+failTypeCode = {
+		'subs': -1,
+		'surf': 0,
+		'uncl': -2,
+		'op'  : 1,
+		'tbd' : -3
+		}
 
 # --- Read the raw data in
 rawData = pd.read_csv('2004-2014_rawFailureData2_prep.csv', engine='python',
@@ -53,16 +58,16 @@ rawData = rawData.append(rawData2)
 # --- Preprocessing
 # Remove some faulty data
 rawData = rawData.drop(
-	index=[datetime(2016, 10, 2), datetime(2016, 10, 3), datetime(2016, 10, 4),
-		   datetime(2016, 10, 5),
-		   datetime(2016, 10, 6)])
+		index=[datetime(2016, 10, 2), datetime(2016, 10, 3), datetime(2016, 10, 4),
+			   datetime(2016, 10, 5),
+			   datetime(2016, 10, 6)])
 rawData = rawData.drop(
-	index=[datetime(2016, 4, 2), datetime(2016, 4, 3), datetime(2016, 4, 4),
-		   datetime(2016, 4, 5),
-		   datetime(2016, 4, 6)])
+		index=[datetime(2016, 4, 2), datetime(2016, 4, 3), datetime(2016, 4, 4),
+			   datetime(2016, 4, 5),
+			   datetime(2016, 4, 6)])
 rawData = rawData.drop(index=[datetime(2013, 4, 2)])
 rawData = rawData.drop(
-	index=pd.date_range(start=datetime(2008, 1, 2), end=datetime(2008, 1, 4)))
+		index=pd.date_range(start=datetime(2008, 1, 2), end=datetime(2008, 1, 4)))
 rawData = rawData.drop(index=[datetime(2009, 10, 4)])
 rawData = rawData.drop(index=[datetime(2011, 8, 14)])
 
@@ -71,13 +76,7 @@ dateList = pd.date_range(rawData.index[0], rawData2.index[-1], freq='D')
 actMatFull = pd.DataFrame(index=dateList, columns=fullWellList)
 
 for date, failInfo in rawData.itertuples():
-	try:
-		surfList, subsList, unclassList = xAndClassFailures(failInfo)
-	except NoWellsFoundError:
-		logging.info(f'No wells found. Date: {date}, failInfo: {failInfo}')
-	except RepeatedWellsError:
-		logging.info(f'Repeated wells found. Date: {date}, failInfo: {failInfo}')
-		continue
+	surfList, subsList, unclassList = xAndClassFailures(date, failInfo)
 
 	actMatFull.loc[date, :] = failTypeCode['op']
 	actMatFull.loc[date, surfList] = failTypeCode['surf']
@@ -89,8 +88,7 @@ for date, failInfo in rawData.itertuples():
 actMatFull = actMatFull.drop(columns=['1408', '752', '1307'])
 
 # Fill in missing data with 1 for the initial few years. The missing data in this
-# period of the data typically means
-# all the wells are running.
+# period of the data typically means all the wells are running.
 actMatFull.iloc[0:1354] = actMatFull.iloc[0:1354].fillna(1)
 
 # Save the data before processing
@@ -98,14 +96,13 @@ actMatFull.to_csv('actMatFull_0_before_proc.csv')
 
 # Upload unprocessed version
 # actMatFull = pd.read_csv('actMatFull_0_before_proc.csv', parse_dates=['Unnamed:
-# 0']).rename(columns={'Unnamed: 0':
-#                                                                                                          'Date'}).set_index('Date')
+# 0']).rename(columns={'Unnamed: 0': 'Date'}).set_index('Date')
 
 
 # Automatic resolution of unclassified errors
 actMatFull = actMatFull.apply(procFailSeries)
 
 # --- Post-processing - Manual corrections of data which do not fit the rules
-import postProcessor
+import actMat_postProcessing
 
 actMatFull.to_csv('actMatFull_0.csv')
